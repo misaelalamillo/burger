1f162301be565d781c6a1ce96f4486e54f5dd9f5
misaelalamillo <misaelalamillo@gmail.com>
2020-03-13 23:49:24 -0500

    hiii

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-//creating the connection to my sql
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Amady$97",
-  database: "burger_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Amady$97",
+    database: "burger_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
