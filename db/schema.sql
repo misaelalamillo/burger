fad803e96f41816fa62c83b533838230c062acf8
misaelalamillo <misaelalamillo@gmail.com>
2020-03-13 22:13:29 -0500

    changed package json to start

--- db/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE burgers (
-id int NOT NULL AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL,
-devoured BOOLEAN DEFAULT false,
-PRIMARY KEY (id)
-);
--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (name) VALUES ('TOMMY BURGER');
-INSERT INTO burgers (name) VALUES ('MISO BURGER');
-INSERT INTO burgers (name, devoured) VALUES ('CHRISTEN BURGER', true);
-INSERT INTO burgers (name, devoured) VALUES ('VANESSA BURGER', true);
-INSERT INTO burgers (name, devoured) VALUES ('BRAIDEN BURGER', true);
-INSERT INTO burgers (name) VALUES ('ERIC BURGER');
