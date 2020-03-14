40764ad3181a24d5d643bd981d5737ae0eaff5f1
misaelalamillo <misaelalamillo@gmail.com>
2020-03-13 22:15:23 -0500

    sent file again

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE burgers (
+id int NOT NULL AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('TOMMY BURGER');
+INSERT INTO burgers (name) VALUES ('MISO BURGER');
+INSERT INTO burgers (name, devoured) VALUES ('CHRISTEN BURGER', true);
+INSERT INTO burgers (name, devoured) VALUES ('VANESSA BURGER', true);
+INSERT INTO burgers (name, devoured) VALUES ('BRAIDEN BURGER', true);
+INSERT INTO burgers (name) VALUES ('ERIC BURGER');
